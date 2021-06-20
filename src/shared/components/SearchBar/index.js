d85cc8430f23e256282b9b9e8ee7245fd23251ee
hkjlee <harry.the.meister@gmail.com>
2021-06-20 21:54:38 +0200

    SearchBar.

---
+++ src/shared/components/SearchBar/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import icons from '../../../assets/images/sprite.svg';
+import styles from './index.module.scss';
+
+const SearchBar = props => {
+  return (
+    <div className={styles.search}>
+      <input type="text" className={styles.searchInput} placeholder="" />
+      <button className={styles.searchButton}>
+        <svg className={styles.searchIcon}>
+          <use href={icons + "#icon-magnifying-glass"}></use>
+        </svg>
+      </button>
+    </div>
+  );
+};
+
+export default SearchBar;
