38ea47d142dddcae8004ddcd183c4ec112976c18
hkjlee <harry.the.meister@gmail.com>
2021-06-24 23:36:29 +0200

    Add icons for menu.

--- src/shared/components/Menu/MenuList.js
+++ src/shared/components/Menu/MenuList.js
@@ -1,3 +1,4 @@
+import icons from '../../../assets/images/sprite.svg';
 import styles from './MenuList.module.scss';
 
 const MenuList = (props) => {
@@ -6,18 +7,33 @@ const MenuList = (props) => {
       <div className={styles.menu}>
         <ul className={styles.menuList}>
           <li className={styles.menuItem}>
-            <a className={styles.menuLink} href="/chats">Chats</a>
-          </li>
-          <li className={styles.menuItem}>
-            <a className={styles.menuLink} href="/local">My Local</a>
-          </li>
-          <li className={styles.menuItem}>
+            <svg className={styles.menuItemIcon}>
+              <use href={icons + "#icon-home"}></use>
+            </svg>
             <a className={styles.menuLink} href="/garage">My Garage</a>
           </li>
           <li className={styles.menuItem}>
+            <svg className={styles.menuItemIcon}>
+              <use href={icons + "#icon-flag"}></use>
+            </svg>
+            <a className={styles.menuLink} href="/local">My Local</a>
+          </li>
+          <li className={styles.menuItem}>
+            <svg className={styles.menuItemIcon}>
+              <use href={icons + "#icon-chat"}></use>
+            </svg>
+            <a className={styles.menuLink} href="/chats">Chats</a>
+          </li>
+          <li className={styles.menuItem}>
+            <svg className={styles.menuItemIcon}>
+              <use href={icons + "#icon-cog"}></use>
+            </svg>
             <a className={styles.menuLink} href="/settings">Settings</a>
           </li>
           <li className={styles.menuItem} onClick={props.onLogoutHandler}>
+            <svg className={styles.menuItemIcon}>
+              <use href={icons + "#icon-log-out"}></use>
+            </svg>
             <a className={styles.menuLink} href="/">Logout</a>
           </li>
         </ul>
