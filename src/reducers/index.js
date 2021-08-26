64144314db5d75fc67b650c1ce680fe14ceb7493
hkjlee <harry.the.meister@gmail.com>
2021-08-26 22:15:42 +0200

    Remove remnant auth logic in redux.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 
-import auth from './auth';
 import chat from './chat';
 import item from './item';
 
 const rootReducer = combineReducers({
-  auth,
   chat,
   item,
 });
